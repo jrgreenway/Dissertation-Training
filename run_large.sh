596ccd35ca837fae1cfe0ec9d62b5d1ee50a0af7
jrgreenway <64894966+jrgreenway@users.noreply.github.com>
2024-08-22 14:18:19 +0100

    bash files for ease

---
+++ run_large.sh
@@ -0,0 +1,3 @@
+python training.py --model bert 
+python training.py --model distilbert 
+python training.py --model xlnet
---
+++ run_medium.sh
@@ -0,0 +1,3 @@
+python training.py --model bert --fraction 0.5
+python training.py --model distilbert --fraction 0.5
+python training.py --model xlnet --fraction 0.5
